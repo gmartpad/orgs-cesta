a8da74153b7eaf915e3ea24d939297da955ab3e7
TI SATC <deptotisatc@gmail.com>
2021-11-02 18:24:11 -0300

    Componente de funcao

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Cesta from './src/telas/Cesta'
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>App react de teste!</Text>
       <Text style={styles.hotReloading}>Ao mudar o texto aqui, o app automaticamente atualiza no dispositivo através do hot reloading!</Text>
+      <Cesta/>
       <StatusBar style="auto" />
     </View>
   );
