21c3691e17fb9a4ea20546df5a6e3c03360b70db
TI SATC <deptotisatc@gmail.com>
2021-11-04 19:45:47 -0300

    feat: Componente de lista

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -12,7 +12,7 @@ const cesta = {
   },
   detalhes: {
     nome: "Cesta de Verduras",
-    logoFazenda: logo,
+    logo,
     nomeFazenda: "Jenny Jack Farm",
     descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
     preco: "R$ 40,00",
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { StyleSheet, Image, Text, Dimensions, View } from 'react-native'
+import { StyleSheet, Image, Text, Dimensions, View, ScrollView } from 'react-native'
 
 import Topo from "./componentes/Topo";
 import Detalhes from './componentes/Detalhes';
-import Texto from "../../componentes/Texto";
+import Itens from './componentes/Itens'
 
-
-export default function Cesta({ topo, detalhes }) {
-    return <>
+export default function Cesta({ topo, detalhes, itens }) {
+    return <ScrollView>
         <Topo {...topo}/>
 
         <View style={estilos.cesta}>
             <Detalhes {...detalhes}/>
+            <Itens {...itens}/>
         </View>
-    </>
+    </ScrollView>
 }
 
 const estilos = StyleSheet.create({
